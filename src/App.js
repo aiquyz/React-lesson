d78140c8edcecd5998775007254d9c786e2695a2
Vlad Myroshnychenko <aiquyz@gmail.com>
2022-10-09 23:11:10 +0300

    Lesson 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef ,useState } from "react";
+import ClassCounter from "./components/ClassCounter";
+import Counter from "./components/Counter";
+import PostItem from "./components/PostItem";
+import PostList from "./components/PostList";
+import MyButton from "./components/UI/Button/MyButton";
+import MyInput from "./components/UI/Input/MyInput";
+import './styles/App.css';
 
 function App() {
+    const [posts, setPosts] = useState([
+      {id:1, title:'Javascript', body:'Description'},
+      {id:2, title:'Javascript 2', body:'Description'},
+      {id:3, title:'Javascript 3', body:'Description'},
+    ])
+    const[post, setPost] = useState({title:'', body:''})
+
+    const addNewPost = (e) => {
+      e.preventDefault()
+      setPosts([...posts, {...post,id:Date.now()}])
+      setPost({title:'', body:''})
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <MyInput 
+          value={post.title}
+          onChange={e => setPost({...post,title: e.target.value})}
+          type="text"
+          placeholder="Название"/>
+        <MyInput 
+        value={post.body}
+        onChange={e => setPost({...post,body:e.target.value})}
+        type="text" 
+        placeholder="Описание"/>
+        <MyButton onClick={addNewPost}>Создать пост</MyButton>
+      </form>
+      <PostList posts={posts} title={'Посты про JS'}/>
     </div>
   );
 }
